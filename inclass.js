1d1eb4b8fb470d6f60061154e97e9eb3bf41e84c
hasan <hasanakoz1986@gmail.com>
2022-09-10 12:02:37 +0300

    first dom practices

---
+++ inclass.js
@@ -0,0 +1,19 @@
+//?********************************************************
+//***************getElementById************************** */
+//************************************************** */
+
+const h1 = document.getElementById("header");
+console.log(h1);
+h1.style.color = "white";
+h1.style.backgroundColor = "red";
+
+//_______________________
+const myInput = document.getElementById("input");
+const button = document.getElementById("btn");
+button.style.backgroundColor = "black";
+button.style.color = "white";
+button.style.width = "7rem";
+button.style.borderRadius = "10px";
+button.style.padding = "1rem";
+myInput.style.padding = "1rem";
+button.style.fontSize = "1.1rem";
