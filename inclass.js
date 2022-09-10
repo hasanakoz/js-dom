1a6dc70352068f85421b0daaa2edfcf5bf65560c
hasan <hasanakoz1986@gmail.com>
2022-09-10 14:13:38 +0300

    traversing dom, creating nodes

--- inclass.js
+++ inclass.js
@@ -86,3 +86,73 @@ lists.forEach((li) => console.log(li.innerText)); //NodeList ile forEach metodu 
 console.log(
   document.querySelectorAll("section ul li:nth-child(4)")[0].innerText
 );
+
+//?********************************************************
+//***************TRAVERSING DOM************************** */
+//************************************************** */
+
+//parentNode
+const listim = document.querySelector(".list");
+const ul = listim.parentNode;
+console.log(ul);
+ul.style.border = "2px solid red";
+
+console.log(listim.parentElement); //parentNode ile benzer
+
+//________________________
+
+const itemListSection = listim.closest("section");
+itemListSection.style.background = "lightgray";
+
+//________________________________
+//children
+const addItem = document.querySelector(".add-item");
+console.log(addItem.children);
+const h2 = addItem.children[0];
+h2.style.color = "pink";
+
+console.log(addItem.parentNode.parentNode.children[0]); //header
+
+console.log(ul.firstElementChild.innerText); //HTML
+console.log(ul.lastElementChild.innerText); //Clarusway
+
+//_______________________
+//nextElementSibling, previousElementSibling
+
+const myListim = ul.children;
+console.log(myListim);
+const javascript = myList[2];
+
+const css = javascript.previousElementSibling;
+console.log(css);
+
+const react = javascript.nextElementSibling;
+console.log(react.textContent);
+
+console.log(javascript.previousElementSibling.previousElementSibling.innerText);
+
+//?********************************************************
+//***************CREATING NODE************************** */
+//************************************************** */
+
+const newP = document.createElement("p");
+newP.id = "new-par";
+newP.setAttribute("class", "new-class");
+const text = document.createTextNode("This is new p element");
+newP.appendChild(text);
+console.log(newP);
+
+const h2N = document.querySelector(".item-list h2");
+h2N.after(newP);
+
+//_________________________
+//getattribute()
+console.log(newP.getAttribute("class")); //new-class
+
+//___________________
+//classList
+
+console.log(newP.classList.contains("par")); //true
+newP.classList.add("new-class");
+console.log(newP);
+newP.classList.remove("new-class");
